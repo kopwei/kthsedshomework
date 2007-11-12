2c7cbba9d8b20b3b03e88bc79fd56fc19af37ef3
kopkop <kopkop@ac96b43e-5339-0410-80b6-0fb156f31913>
2007-11-12 21:42:09 +0000

    Check ClientServerComm project into repository

---
+++ trunk/ClientServerComm/src/message/MessageType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package message;
+
+/**
+ *
+ * @author Kop
+ */
+public enum MessageType {
+    UnknownMessage,
+    RequestMessage,
+    InformMessage;    
+}

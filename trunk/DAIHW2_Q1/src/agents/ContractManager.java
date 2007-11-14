2c0b28d68fb883a271d49ca7574d824073632bb8
kopkop <kopkop@ac96b43e-5339-0410-80b6-0fb156f31913>
2007-11-14 19:10:36 +0000

    Add the DAI Homework2 Q1 to trunk

---
+++ trunk/DAIHW2_Q1/src/agents/ContractManager.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package agents;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author Kop
+ */
+public class ContractManager extends Agent{
+    
+
+}

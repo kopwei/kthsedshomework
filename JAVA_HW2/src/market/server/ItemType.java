1cd3dfc0b9af126222f9629220c265107d925c2c
kopkop <kopkop@ac96b43e-5339-0410-80b6-0fb156f31913>
2007-11-19 17:26:16 +0000

    Implement some code of the Java homework 2

---
+++ JAVA_HW2/src/market/server/ItemType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package market.server;
+
+/**
+ *
+ * @author Kop
+ */
+public enum ItemType {
+    Unknown,
+    Food,
+    Electronic,
+    Furniture,
+    Clothes
+}

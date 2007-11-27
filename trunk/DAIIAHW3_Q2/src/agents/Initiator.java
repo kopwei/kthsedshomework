a4c503c9aaf2c140f02209276c4a2d6a01182d1e
kopkop <kopkop@ac96b43e-5339-0410-80b6-0fb156f31913>
2007-11-27 01:43:19 +0000

    Add the initiator and pariticipant agent into the project

---
+++ trunk/DAIIAHW3_Q2/src/agents/Initiator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package agents;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author Kop
+ */
+public class Initiator extends Agent{
+
+}

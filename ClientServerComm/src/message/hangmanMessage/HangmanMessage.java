95243f2282a6f82366178d4f3984ead9d1952ea1
kopkop <kopkop@ac96b43e-5339-0410-80b6-0fb156f31913>
2007-12-01 02:03:17 +0000

    correct some bug of hangman message

--- ClientServerComm/src/message/hangmanMessage/HangmanMessage.java
+++ ClientServerComm/src/message/hangmanMessage/HangmanMessage.java
@@ -20,6 +20,11 @@ import message.SimpleMessage;
 public class HangmanMessage extends SimpleMessage implements Persistent{
     private HangmanMessageType messageType = HangmanMessageType.Unknown;
     
+    public HangmanMessage() {
+        super();
+        messageType = HangmanMessageType.Unknown;
+    }
+    
     public void setHangmanMessageType(HangmanMessageType type) { this.messageType = type; }
     public HangmanMessageType getHangmanMessageType() { return messageType; }
 
@@ -28,7 +33,9 @@ public class HangmanMessage extends SimpleMessage implements Persistent{
         DataOutputStream dout = new DataOutputStream(bout);
         
         dout.writeUTF(messageType.toString());
+        //System.out.println("The message type wrote is " + messageType.toString());
         dout.writeUTF(this.getContent());
+        //System.out.println("The content wrote is " + this.getContent());
         dout.flush();
         return bout.toByteArray();
     }
@@ -36,10 +43,12 @@ public class HangmanMessage extends SimpleMessage implements Persistent{
     public void resurrect(byte[] data) throws IOException {
         ByteArrayInputStream bin = new ByteArrayInputStream(data);
         DataInputStream din = new DataInputStream(bin);
-        setContent(din.readUTF());
-        messageType = HangmanMessageType.valueOf(din.readUTF());
+        
+        String type = din.readUTF();
+        //System.out.println("The type is " + type);
+        String content = din.readUTF();
+        setContent(content);
+        //System.out.println("The content is " + content);
+        messageType = HangmanMessageType.valueOf(type);
     }
-    
-    
-
 }

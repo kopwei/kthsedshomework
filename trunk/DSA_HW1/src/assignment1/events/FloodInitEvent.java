05855bf709f4afc5bd658ea94327fe91185ea48e
ricky.yanyi326 <ricky.yanyi326@ac96b43e-5339-0410-80b6-0fb156f31913>
2008-02-01 20:07:44 +0000

    FloodInitEvent.java

--- trunk/DSA_HW1/src/assignment1/events/FloodInitEvent.java
+++ trunk/DSA_HW1/src/assignment1/events/FloodInitEvent.java
@@ -9,8 +9,13 @@ package assignment1.events;
  * @author Kop
  */
 public class FloodInitEvent implements tbn.api.Event {
+    private String eventMessage;
 
     public FloodInitEvent(String eventMessage) {
-        
+        this.eventMessage = eventMessage;
+    }
+    
+    public String getEventMessage() {
+        return eventMessage;
     }
 }

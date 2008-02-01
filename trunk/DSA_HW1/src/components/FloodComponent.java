f7684f8d84d28a9bffab6c8036cc26452cff34da
kopkop <kopkop@ac96b43e-5339-0410-80b6-0fb156f31913>
2008-02-01 21:48:29 +0000

    Complete the flood message handler function

--- trunk/DSA_HW1/src/components/FloodComponent.java
+++ trunk/DSA_HW1/src/components/FloodComponent.java
@@ -2,13 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package components;
 
+import assignment1.events.FloodDoneEvent;
 import assignment1.events.FloodInitEvent;
 import assignment1.events.FloodMessage;
 import assignment1.events.InitEvent;
 import assignments.util.TopologyDescriptor;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Hashtable;
 import org.apache.log4j.Logger;
 import tbn.api.Component;
 import tbn.comm.mina.MessageHandler;
@@ -24,10 +27,12 @@ public class FloodComponent {
     private Component component;
     private TopologyDescriptor topologyDescriptor;
     private MessageHandler messageHandler;
+    private Hashtable<FloodMessage, HashSet<NodeReference>> floodMessageTable;
 
     public FloodComponent(Component component) {
         this.component = component;
         messageHandler = new MessageHandler(this.component);
+        floodMessageTable = new Hashtable<FloodMessage, HashSet<NodeReference>>();
     }
 
     public void handleInitEvent(InitEvent event) {
@@ -48,8 +53,34 @@ public class FloodComponent {
             component.raiseEvent(floodMessage);
         }
     }
-    
+
     public void handleFloodMessage(FloodMessage event) {
-        
+        // If the message is a totally new message, we have to store it and its source
+        if (!floodMessageTable.containsKey(event)) {
+            HashSet<NodeReference> sourceSet = new HashSet<NodeReference>();
+            sourceSet.add(event.getSource());
+            floodMessageTable.put(event, sourceSet);
+            // We have to send the event to all other neighbors
+            for (NodeReference neighbor : topologyDescriptor.getAllOtherNodes()) {
+                event.setDestination(neighbor);
+                component.raiseEvent(event);
+            }
+        } // If the message is not new message, we have to store its source
+        else {
+            HashSet<NodeReference> nodeSet = floodMessageTable.get(event);
+            nodeSet.add(event.getSource());
+            // If the source number reaches the number of neighbors, we thought it is done
+            if (nodeSet.size() == topologyDescriptor.getAllOtherNodes().size()) {
+                // Clear the flood table
+                Enumeration<HashSet<NodeReference>> enumSets = floodMessageTable.elements();
+                while (enumSets.hasMoreElements()) {
+                    HashSet<tbn.comm.mina.NodeReference> hashSet = (HashSet<tbn.comm.mina.NodeReference>) enumSets.nextElement();
+                    hashSet.clear();
+                }
+                floodMessageTable.clear();
+                FloodDoneEvent doneEvent = new FloodDoneEvent();
+                component.raiseEvent(doneEvent);
+            }
+        }
     }
 }

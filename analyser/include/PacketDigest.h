387d2ea444c961416d8e4eb73421ba3f102119da
kopkop <kopkop@ac96b43e-5339-0410-80b6-0fb156f31913>
2008-06-27 11:43:34 +0000

    Add some statistic classes

---
+++ analyser/include/PacketDigest.h
@@ -0,0 +1,56 @@
+/**
+ *   PacketDigest.h	
+ *   Author: Wei Zhenfang                                                  
+ *                                                                         
+ *   This program is free software; you can redistribute it and/or modify  
+ *   it under the terms of the GNU General Public License as published by  
+ *   the Free Software Foundation; either version 2 of the License, or     
+ *   (at your option) any later version.                                   
+ *                                                                         
+ *   This program is distributed in the hope that it will be useful,       
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
+ *   GNU General Public License for more details.                          
+ *                                                                         
+ *   You should have received a copy of the GNU General Public License     
+ *   along with this program; if not, write to the                         
+ *   Copyright (C) 2008 by Ericsson                                        
+ */
+
+#pragma once;
+
+#ifndef _PACKET_DIGEST_
+#define _PACKET_DIGEST_
+
+#include <net\socket.h>
+
+
+//////////////////////////////////////////////////////////////////////////
+
+/**
+ *	This class represent a digest of an IP packet
+ */
+class CPacketDigest
+{
+public:
+	CPacketDigest(struct pcap_pkthdr* header, const char* packet);
+	~CPacketDigest(void);
+
+	const time_t			getTimeStamp() const;
+	in_addr*				getSrcAddress() const;
+	in_addr*				getDestAddress() const;
+	unsigned int			getPacketSize() const;
+	unsigned short			getProtocol() const;
+	unsigned short			getProtocolClassification() const;
+
+private:
+	time_t				m_timeStamp;		// Time
+	in_addr*			m_pSrcIPAddr;		// Source IP Address
+	in_addr*			m_pDestIPAddr;		// Destination IP Address
+	unsigned int		m_sPacketSize;		// Packet size
+	unsigned short		m_sProtocol;		// Protocol
+	unsigned short		m_sClass;			// Protocol classification
+	
+};
+
+#endif
---
+++ analyser/include/PacketStatistician.h
@@ -0,0 +1,55 @@
+/**
+ *   PacketStatistician.h	
+ *   Author: Wei Zhenfang                                                  
+ *                                                                         
+ *   This program is free software; you can redistribute it and/or modify  
+ *   it under the terms of the GNU General Public License as published by  
+ *   the Free Software Foundation; either version 2 of the License, or     
+ *   (at your option) any later version.                                   
+ *                                                                         
+ *   This program is distributed in the hope that it will be useful,       
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
+ *   GNU General Public License for more details.                          
+ *                                                                         
+ *   You should have received a copy of the GNU General Public License     
+ *   along with this program; if not, write to the                         
+ *   Copyright (C) 2008 by Ericsson                                        
+ */
+
+#pragma once;
+
+#ifndef _PACKET_STATISTICIAN_
+#define _PACKET_STATISTICIAN_
+
+
+#include <map>
+#include "analyserpxAggreg.h"
+#include "PacketStatistic.h"
+#include "SubscriberStatistic.h"
+
+using namespace std;
+//////////////////////////////////////////////////////////////////////////
+/**
+ *	This class is a class used for analyzing the packets
+ */
+class CPacketStatistician
+{
+	
+
+public:
+	CPacketStatistician(void);
+	~CPacketStatistician(void);
+
+	void processNewPacket(unsigned char *args, const struct pcap_pkthdr *header, const u_char *packet, ThreadParams *tp);
+
+private:
+	
+	CPacketStatistic		m_totalPacketStatistic;
+	CSubscriberStatistic	m_subscriberStatistic;
+	//map<__int6>
+
+};
+
+
+#endif
